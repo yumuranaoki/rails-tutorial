e2e030e732d0582d4d1344600dd2e1f296c56bbb
Morinaga-33 <morinaga@sansan.com>
2019-06-27 12:13:01 +0900

    登録後にリダイレクトするように

--- spec/requests/users_signups_spec.rb
+++ spec/requests/users_signups_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "UsersSignups", type: :request do
-  describe "GET /users_signups" do
+  describe "GET /users_signup" do
     it "works! (now write some real specs)" do
-      get users_signups_path
+      get signup_path
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
       expect(response).to have_http_status(200)
     end
   end
